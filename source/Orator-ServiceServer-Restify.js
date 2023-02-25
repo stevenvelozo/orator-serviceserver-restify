676675532de33eabf16bc89976453955e705488f
Steven Velozo <steven@velozo.com>
2023-02-25 15:08:56 -0800

    Fixing and adding other verbs; working test harness with plugin pattern.

--- source/Orator-ServiceServer-Restify.js
+++ source/Orator-ServiceServer-Restify.js
@@ -7,7 +7,7 @@ class OratorServiceServerRestify extends libOratorServiceServerBase
 	{
 		super(pOrator);
 
-		this.server = restify.createServer();			
+		this.server = libRestify.createServer();			
 	}
 
 	/*
@@ -18,7 +18,7 @@ class OratorServiceServerRestify extends libOratorServiceServerBase
 		this.server.listen(pPort, (pError) =>
 			{
 				this.Active = true;
-				this.log.info(`RESTIFY server [${server.name}] listening on [${server.url}].`);
+				this.log.info(`RESTIFY server [${this.server.name}] listening on [${this.server.url}].`);
 				return fCallback(pError);
 			});
 	}
@@ -44,7 +44,73 @@ class OratorServiceServerRestify extends libOratorServiceServerBase
 			return false;
 		}
 
-		return this.server.get('/hello/:name', ...fRouteProcessingFunctions);
+		return this.server.get(pRoute, ...fRouteProcessingFunctions);
+	}
+
+	put(pRoute, ...fRouteProcessingFunctions)
+	{
+		if (!super.put(pRoute, ...fRouteProcessingFunctions))
+		{
+			this.log.error(`RESTIFY provider failed to map PUT route [${pRoute}]!`);
+			return false;
+		}
+
+		return this.server.put(pRoute, ...fRouteProcessingFunctions);
+	}
+
+	post(pRoute, ...fRouteProcessingFunctions)
+	{
+		if (!super.post(pRoute, ...fRouteProcessingFunctions))
+		{
+			this.log.error(`RESTIFY provider failed to map POST route [${pRoute}]!`);
+			return false;
+		}
+
+		return this.server.post(pRoute, ...fRouteProcessingFunctions);
+	}
+
+	del(pRoute, ...fRouteProcessingFunctions)
+	{
+		if (!super.del(pRoute, ...fRouteProcessingFunctions))
+		{
+			this.log.error(`RESTIFY provider failed to map DEL route [${pRoute}]!`);
+			return false;
+		}
+
+		return this.server.del(pRoute, ...fRouteProcessingFunctions);
+	}
+
+	patch(pRoute, ...fRouteProcessingFunctions)
+	{
+		if (!super.patch(pRoute, ...fRouteProcessingFunctions))
+		{
+			this.log.error(`RESTIFY provider failed to map PATCH route [${pRoute}]!`);
+			return false;
+		}
+
+		return this.server.patch(pRoute, ...fRouteProcessingFunctions);
+	}
+
+	opts(pRoute, ...fRouteProcessingFunctions)
+	{
+		if (!super.opts(pRoute, ...fRouteProcessingFunctions))
+		{
+			this.log.error(`RESTIFY provider failed to map OPTS route [${pRoute}]!`);
+			return false;
+		}
+
+		return this.server.opts(pRoute, ...fRouteProcessingFunctions);
+	}
+
+	head(pRoute, ...fRouteProcessingFunctions)
+	{
+		if (!super.head(pRoute, ...fRouteProcessingFunctions))
+		{
+			this.log.error(`RESTIFY provider failed to map HEAD route [${pRoute}]!`);
+			return false;
+		}
+
+		return this.server.head(pRoute, ...fRouteProcessingFunctions);
 	}
 
 	/*************************************************************************
