9079673ffbd5c895bad51266f68f93867c5af0ce
Alex Decker <alex.decker@headlight.com>
2026-02-25 13:21:11 -0800

    Add types.  2.0.8

--- source/Orator-ServiceServer-Restify.js
+++ source/Orator-ServiceServer-Restify.js
@@ -7,6 +7,11 @@ const _DefaultRestifyConfiguration =
 };
 class OratorServiceServerRestify extends libOratorServiceServerBase
 {
+	/**
+	 * @param {import('fable')|Record<string, any>} [pFable] - (optional) The fable instance, or the options object if there is no fable
+	 * @param {Record<string, any>|string} [pOptions] - (optional) The options object, or the service hash if there is no fable
+	 * @param {string} [pServiceHash] - (optional) The service hash to identify this service instance
+	 */
 	constructor(pFable, pOptions, pServiceHash)
 	{
         super(pFable, pOptions, pServiceHash);
@@ -17,12 +22,19 @@ class OratorServiceServerRestify extends libOratorServiceServerBase
 			(this.fable.settings.hasOwnProperty('RestifyConfiguration')) ? this.fable.settings.RestifyConfiguration :
 			{};
 
+		/** @type {libRestify} */
 		this.server = libRestify.createServer(Object.assign({}, _DefaultRestifyConfiguration, tmpRestifyConfiguration));
 	}
 
 	/*
 	 * Service Lifecycle Functions
 	 *************************************************************************/
+	/**
+	 * Listens for network calls on the specified port (or starts a virtual serviceserver for that "port").
+	 * @param {number} pPort - The port number to listen on.
+	 * @param {(pError?: Error) => void} fCallback - The callback function to execute after listening.
+	 * @return {any} The result of the callback function.
+	 */
 	listen(pPort, fCallback)
 	{
 		this.server.listen(pPort, (pError) =>
@@ -34,6 +46,12 @@ class OratorServiceServerRestify extends libOratorServiceServerBase
 			});
 	}
 
+	/**
+	 * Closes the service server.
+	 *
+	 * @param {(pError?: Error) => any} fCallback - The callback function to be executed after closing the server.
+	 * @return {any} - The result of the callback function.
+	 */
 	close(fCallback)
 	{
 		this.server.close((pError) =>
@@ -47,6 +65,12 @@ class OratorServiceServerRestify extends libOratorServiceServerBase
 	 * End of Service Lifecycle Functions
 	 */
 
+	/**
+	 * Registers a global handler function to be used by the Orator service server.
+	 *
+	 * @param {import('orator-serviceserver-base').RequestHandler} fHandlerFunction - The handler function to be registered. It should have the prototype function(Request, Response, Next).
+	 * @returns {any} - The result of adding the route to the concrete service provider (ex. a route object, a boolean).
+	 */
 	use(fHandlerFunction)
 	{
 		if (!super.use(fHandlerFunction))
@@ -58,6 +82,12 @@ class OratorServiceServerRestify extends libOratorServiceServerBase
 		this.server.use(fHandlerFunction);
 	}
 
+	/**
+	 * Registers a global handler function to be used by the Orator service server that runs before routing.
+	 *
+	 * @param {function} fHandlerFunction - The handler function to be registered. It should have the prototype function(Request, Response, Next).
+	 * @returns {any} - The result of adding the route to the concrete service provider (ex. a route object, a boolean).
+	 */
 	pre(fHandlerFunction)
 	{
 		if (!super.pre(fHandlerFunction))
@@ -69,11 +99,17 @@ class OratorServiceServerRestify extends libOratorServiceServerBase
 		this.server.pre(fHandlerFunction);
 	}
 
-	bodyParser()
+	/**
+	 * Middleware function for parsing the request body.
+	 *
+	 * @param {Record<string, any>} [pOptions] - The options for the body parser.
+	 * @return {import('orator-serviceserver-base').RequestHandler} - The middleware function.
+	 */
+	bodyParser(pOptions)
 	{
 		// Restify has a built-in bodyParser plugin that returns a standard
 		// f(Request, Response, Next) => {}; function.
-		return libRestify.plugins.bodyParser(
+		return libRestify.plugins.bodyParser(Object.assign(
 			{
 				maxBodySize: 0,
 				mapParams: false,
@@ -97,42 +133,91 @@ class OratorServiceServerRestify extends libOratorServiceServerBase
 */
 				multiples: true,
 				hash: 'sha1'
-			});
+			}, pOptions));
 	}
 
 	/*
 	 * Service Route Functions
 	 *************************************************************************/
+	/**
+	 * Handles HTTP GET requests -- this is a base function that does nothing; override by the serviceserver is expected.
+	 *
+	 * @param {string} pRoute - The route of the request.
+	 * @param {...Function} fRouteProcessingFunctions - The processing functions for the route.
+	 * @returns {any} - The result of adding the route to the concrete service provider (ex. a route object, a boolean).
+	 */
 	doGet(pRoute, ...fRouteProcessingFunctions)
 	{
 		return this.server.get(pRoute, ...fRouteProcessingFunctions);
 	}
 
+	/**
+	 * Handles HTTP PUT requests -- this is a base function that does nothing; override by the serviceserver is expected.
+	 *
+	 * @param {string} pRoute - The route to handle.
+	 * @param {...Function} fRouteProcessingFunctions - The processing functions to execute for the route.
+	 * @returns {any} - The result of adding the route to the concrete service provider (ex. a route object, a boolean).
+	 */
 	doPut(pRoute, ...fRouteProcessingFunctions)
 	{
 		return this.server.put(pRoute, ...fRouteProcessingFunctions);
 	}
 
+	/**
+	 * Handles the HTTP POST request for a specific route.
+	 *
+	 * @param {string} pRoute - The route to handle the POST request for.
+	 * @param {...Function} fRouteProcessingFunctions - The processing functions to execute for the route.
+	 * @returns {any} - The result of adding the route to the concrete service provider (ex. a route object, a boolean).
+	 */
 	doPost(pRoute, ...fRouteProcessingFunctions)
 	{
 		return this.server.post(pRoute, ...fRouteProcessingFunctions);
 	}
 
+	/**
+	 * Handles HTTP DELETE requests -- this is a base function that does nothing; override by the serviceserver is expected.
+	 *
+	 * @param {string} pRoute - The route of the resource to delete.
+	 * @param {...Function} fRouteProcessingFunctions - The processing functions to be executed to delete the resource.
+	 * @returns {any} - The result of adding the route to the concrete service provider (ex. a route object, a boolean).
+	 */
 	doDel(pRoute, ...fRouteProcessingFunctions)
 	{
 		return this.server.del(pRoute, ...fRouteProcessingFunctions);
 	}
 
+	/**
+	 * Handles HTTP PATCH requests -- this is a base function that does nothing; override by the serviceserver is expected.
+	 *
+	 * @param {string} pRoute - The route to send the PATCH request to.
+	 * @param {...Function} fRouteProcessingFunctions - The processing functions to apply to the route.
+	 * @returns {any} - The result of adding the route to the concrete service provider (ex. a route object, a boolean).
+	 */
 	doPatch(pRoute, ...fRouteProcessingFunctions)
 	{
 		return this.server.patch(pRoute, ...fRouteProcessingFunctions);
 	}
 
+	/**
+	 * Handles HTTP OPT requests -- this is a base function that does nothing; override by the serviceserver is expected.
+	 *
+	 * @param {string} pRoute - The route.
+	 * @param {...Function} fRouteProcessingFunctions - The processing functions to apply to the route.
+	 * @returns {any} - The result of adding the route to the concrete service provider (ex. a route object, a boolean).
+	 */
 	doOpts(pRoute, ...fRouteProcessingFunctions)
 	{
 		return this.server.opts(pRoute, ...fRouteProcessingFunctions);
 	}
 
+	/**
+	 * Handles HTTP HEAD requests -- this is a base function that does nothing; override by the serviceserver is expected.
+	 *
+	 * @param {string} pRoute - The route to handle the HEAD request for.
+	 * @param {...Function} fRouteProcessingFunctions - The processing functions to execute for the route.
+	 * @returns {any} - The result of adding the route to the concrete service provider (ex. a route object, a boolean).
+	 */
 	doHead(pRoute, ...fRouteProcessingFunctions)
 	{
 		return this.server.head(pRoute, ...fRouteProcessingFunctions);
---
+++ types/Orator-ServiceServer-Restify.d.ts
@@ -0,0 +1,7 @@
+export = OratorServiceServerRestify;
+declare class OratorServiceServerRestify extends libOratorServiceServerBase {
+    /** @type {libRestify} */
+    server: libRestify;
+}
+import libOratorServiceServerBase = require("orator-serviceserver-base");
+//# sourceMappingURL=Orator-ServiceServer-Restify.d.ts.map
